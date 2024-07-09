a84e4ee1f68960d596b2965a5128a6a76f8da615
unknown <nika.chichua26@gmail.com>
2024-07-09 14:54:55 +0400

    feat: router init

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,14 @@ import { Route, Routes } from "react-router-dom";
 
 function App() {
   const HomePage = lazy(() => import("@src/views/HomePage"));
+  const PostPage = lazy(() => import("@src/views/PostPage"));
 
   return (
     <Suspense fallback={<Loading />}>
       <Routes>
         <Route element={<PublicLayout />}>
           <Route path="/" element={<HomePage />} />
+          <Route path="/posts/:id" element={<PostPage />} />
         </Route>
       </Routes>
     </Suspense>
